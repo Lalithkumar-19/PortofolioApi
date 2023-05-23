42b20f659c78ba3e74f52d60d04ddf150ee1413a
M.Lalikth kumar <121875934+Lalithkumar-19@users.noreply.github.com>
2023-05-23 10:27:57 +0530

    Update index.js
    updated server

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const multer = require("multer");
 const Admin = require("./models/Admin");
 const UploadMiddleware = multer({ dest: 'uploads/' })
 const jwt = require("jsonwebtoken");
+const PORT=2000||process.env.port;
 mongoose.set('strictQuery', false)
 const app = express();
 require("dotenv").config();
@@ -16,57 +17,6 @@ app.use(express.json());
 app.use(cookieparser())
 app.use('/uploads', express.static(__dirname + "/uploads"));
 mongoose.connect(process.env.Mongo_Url).then(() => console.log("connected to db")).catch(err => console.log(err))
-app.get("/", (req, res) => {
-    res.send("hello lalith kumar!!")
-})
-app.post("/card", UploadMiddleware.single('file'), async (req, res) => {
-    const {token} = req.body.token?req.body:null;
-    console.log("token", token);
-    try {
-        if (token) {
-            jwt.verify(token, process.env.secret, {}, async (err, info) => {
-                if (err) throw err;
-    
-                const { originalname, path } = req.file;
-                const parts = originalname.split('.');
-                const ext = parts[parts.length - 1];
-                const newpath = path + '.' + ext;
-                fs.renameSync(path, newpath);
-                const { code, view, content } = req.body;
-                const postDoc = await Cart.create({
-                    img: newpath,
-                    content: content,
-                    code: code,
-                    view: view,
-
-                })
-                res.status(200).json("uploaded successfully !!!")
-                console.log(postDoc);
-
-            })
-
-        }
-        else {
-            res.status(500).json("unauthorized request to made changes");
-        }
-
-
-
-    } catch (err) {
-        if (err) throw err;
-        res.status(500).json('not created');
-    }
-})
-
-
-app.get("/cart", async (req, res) => {
-    console.log("cart root is called");
-    const CartData = await Cart.find();
-    res.json(CartData);
-console.log(CartData)
-
-
-})
 
 app.post("/login", async (req, res) => {
     console.log("login route is  called!!!")
@@ -84,7 +34,8 @@ app.post("/login", async (req, res) => {
             const admindoc = await Admin.findOne({ adminemail: email })
             if (admindoc) {
                 if (admindoc.adminemail === email && admindoc.adminpassword === password) {
-                    jwt.sign({ email, password }, process.env.secret, {}, (err, token) => {
+                    jwt.sign(
+                        { email, password },process.env.secret,{}, (err, token) => {
                         if (err) throw err;
                         res.status(200).cookie('token', token).json({
                             token: token,
@@ -111,21 +62,79 @@ app.post("/login", async (req, res) => {
 })
 
 
+app.get("/", (req, res) => {
+    res.send("hello lalith kumar!!")
+})
+
+
+app.post("/card", UploadMiddleware.single('file'), async (req, res) => {
+    const {token} = req.body;
+    console.log("token", token);
+    try {
+        if (token) {
+            jwt.verify(token,process.env.secret,{},async(err, info) => {
+                if (err) throw err;
+                const { originalname, path } = req.file;
+                const parts = originalname.split('.');
+                const ext = parts[parts.length - 1];
+                const newpath = path + '.' + ext;
+                fs.renameSync(path, newpath);
+                const { code, view, content,skills } = req.body;
+                const postDoc = await Cart.create({
+                    img: newpath,
+                    content: content,
+                    code: code,
+                    view: view,
+                    skills:skills
+
+                })
+                res.status(200).json("uploaded successfully !!!")
+                console.log(postDoc);
+
+            })
+
+        }
+        else {
+            res.status(500).json("unauthorized request to made changes");
+        }
+
+
+
+    } catch (err) {
+        if (err) throw err;
+        res.status(500).json('not created');
+    }
+})
+
+app.get("/cart", async (req, res) => {
+    console.log("cart root is called");
+    const CartData = (await Cart.find().sort({"created_at":-1}));
+    res.json(CartData);
+console.log(CartData)
+
+
+})
+
+
+
 
 app.post("/deleteitem", async(req, res) => {
     console.log("deleted called");
     const { id, img, token } = req.body;
+    console.log(img);
     console.log(token);
     try {
         if (token) {
             jwt.verify(token, process.env.secret, {}, async (err, info) => {
                 if (err) throw err;
                 if (info) {
-                    fs.unlink(__dirname + '/' + img, (err) => {
-                        if (err) throw err;
-                        console.log("image deleted successfully!")
-                    });
-
+                    console.log("image unlink address is::--",__dirname+"/"+img);
+            
+                        fs.unlink(__dirname +"/"+img, (err) => {
+                            if (err) throw err;
+                            console.log("image deleted successfully!")
+                        });
+                   
                     const cartdet = await Cart.findById(id);
                     await Cart.deleteOne({ _id: cartdet.id }).then((suc) => {
                         res.status(200).json("deleted sucessfully")
@@ -152,4 +161,4 @@ app.post("/deleteitem", async(req, res) => {
 
 
 
-app.listen(2000, () => console.log("connected to port 2000"))
+app.listen(PORT, () => console.log(`connected to port ${PORT}`))
